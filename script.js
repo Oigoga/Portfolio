403032e7f09706621281353de4b60a2986b24a03
oigoga <gogaelisabeth21@gmail.com>
2022-08-25 23:29:09 +0100

    Changed the home page design

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("menu").addEventListener("click", function() {
+    document.getElementById("nav").classList.toggle("-right-full")
+    document.getElementById("nav").classList.toggle("-right-0")
+});
